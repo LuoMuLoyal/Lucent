50241fd7c46bd0a6407b005c497df3005628ea18
LuoMuLoyal <2508015296@qq.com>
2026-06-06 11:54:58 +0800

    feat(admin): 集成AdminJS后台管理面板

---
+++ src/admin/admin.module.ts
@@ -0,0 +1,5 @@
+import { Module } from '@nestjs/common';
+
+@Module({})
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class -- NestJS @Module requires a class declaration
+export class AdminPanelModule {}
