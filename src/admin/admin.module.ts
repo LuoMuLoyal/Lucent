4a93d930fd6d25ec906b0675eff3576d96170726
LuoMuLoyal <2508015296@qq.com>
2026-06-06 11:58:25 +0800

    revert: 移除AdminJS集成，改用Prisma Studio

--- src/admin/admin.module.ts
+++
@@ -1,5 +0,0 @@
-import { Module } from '@nestjs/common';
-
-@Module({})
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class -- NestJS @Module requires a class declaration
-export class AdminPanelModule {}
